8be2c045c1a2fe5d87e5d2af858d8822941e9e14
ssokkara159 <cdn.shiva13@gmail.com>
2024-02-02 15:17:52 +0530

    body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Login from "./Login";
 import Browser from "./Browser";
+import { createBrowserRouter } from "react-router-dom";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+    },
+
+    {
+      path: "/Browser",
+      element: <Browser />,
+    },
+  ]);
+
   return (
     <div>
       <Login />
