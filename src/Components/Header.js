e4cfc6d52e71397c4262c8c8154d82dc7c4b69f8
ssokkara159 <cdn.shiva13@gmail.com>
2024-02-19 17:40:33 +0530

    adding logo and bg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img
+        src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+        alt="logo"
+      />
+    </div>
+  );
 };
 
 export default Header;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return <div>Login</div>;
